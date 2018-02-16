7af09a4422667de2a4da0383648ad3c40a36bcd3
ruslanvs <ruslanvs@yahoo.com>
2018-02-15 20:32:25 -0800

    Initial file setup

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require( "express" );
+let app = express();
+
+let bodyParser = require( "body-parser" );
+let path = require( "path" );
+let mongoose = require( "mongoose" );
+
+mongoose.Promise = global.Promise;
+
+app.use( bodyParser.urlencoded( {extended: true} ) );
+app.use( bodyParser.json() );
+
+app.use( express.static( __dirname + "/client/dist" ) );
+
+mongoose.connect( "mongodb://localhost/restful_tasks" );
+
+app.listen( 8000, function(){
+    console.log( "listening on port 8000" );
+} );
